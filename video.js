52f21b357b7b7d23d6e2e49f86f52fb93f95fa49
brandonaaskov <brandonaaskov@gmail.com>
2015-02-13 21:29:59 -0800

    Trying out exec.

--- video.js
+++ video.js
@@ -1,17 +1,27 @@
 var RaspiCam = require('raspicam')
+var exec = require('child_process').exec
 
-var video = new RaspiCam({
-  mode: 'video',
-  output: './videos/file-' + Date.now() + '.mov',
-  c: ''
-})
+//var video = new RaspiCam({
+//  mode: 'video',
+//  output: './videos/file-' + Date.now() + '.mov',
+//  c: ''
+//})
+//
+//video.on('read', function(err, timestamp, filename){
+//  console.log('video saved', filename)
+//})
+//
+//video.on('data', function(data) {
+//  console.log('video data buffer', data)
+//})
 
-video.on('read', function(err, timestamp, filename){
-  console.log('video saved', filename)
-})
-
-video.on('data', function(data) {
-  console.log('video data buffer', data)
-})
+var video = {
+  start: function () {
+    var onVideoBuffer = function (error, stdout, stderr) {
+      console.log('video buffer')
+    }
+    exec('raspivid -o ./videos/test.mov', onVideoBuffer)
+  }
+}
 
 module.exports = video
