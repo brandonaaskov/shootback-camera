66a2524c3e2776361ef0f1bc7a172f78da33c726
Brandon Aaskov <brandonaaskov@gmail.com>
2021-05-17 22:55:16 -0400

    using ffmpeg to wrap h264 stream in valid container format

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const fs = require('fs')
 const { StreamCamera, Codec } = require('pi-camera-connect')
+const ffmpeg = require('fluent-ffmpeg')
 
 const streamCamera = new StreamCamera({ codec: Codec.H264 })
-const writeStream = fs.createWriteStream('video-stream.h264')
+const writeStream = fs.createWriteStream('video.h264')
 const videoStream = streamCamera.createStream()
 
 videoStream.pipe(writeStream)
 
 streamCamera.startCapture().then(() => {
-  setTimeout(() => streamCamera.stopCapture(), 5000)
+  setTimeout(() => {
+    streamCamera.stopCapture()
+    
+    const inFilename = 'video.h264'
+    const outFilename = 'video.mp4'
+
+    ffmpeg(inFilename)
+      .outputOptions("-c:v", "copy") // this will copy the data instead or reencode it
+      .save(outFilename)
+  }, 5000)
 })
